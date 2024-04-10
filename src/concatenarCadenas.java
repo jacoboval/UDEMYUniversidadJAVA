c9ecf69e0ee91b09ae20811f60513fb4658af305
jacoboval <jacoboval@gmail.com>
2024-04-10 15:59:25 -0600

    se agrega update

---
+++ src/concatenarCadenas.java
@@ -0,0 +1,11 @@
+public class concatenarCadenas {
+    public static void main(String[] args) {
+        var usuer = "java";
+        var titulo = "Admin";
+        System.out.println(usuer + titulo);
+        var i = 3;
+        var j = 4;
+        System.out.println(j + i);
+        System.out.println(usuer + i);
+    }
+}
---
+++ src/variables.java
@@ -0,0 +1,20 @@
+public class variables {
+    public static void main(String[] args) {
+        int miVariableEntera = 20;
+        System.out.println(miVariableEntera);
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String nombre ="Jacobo";
+        String apellido = "Valencia";
+        int edad = 45;
+        double peso = 62.5;
+        var fechaNac = 1978;
+        var ocupacion = "Estudiante";
+        var arbol = 135453;
+        System.out.println(nombre + " " +  apellido + "\n" + "Edad: " + edad + " " + "Peso: "+ peso );
+        System.out.println("\n Fecha de Nacimiento: " + fechaNac);
+        System.out.println("\n Ocupacion: "+ ocupacion);
+        System.out.println(arbol);
+    }
+}
